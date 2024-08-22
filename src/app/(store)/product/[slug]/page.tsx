fb560cfde358555b7701908b78b28ba07887b23e
artprange <arthur.prange@gmail.com>
2024-08-22 12:56:09 -0300

    static build

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,27 +1,21 @@
 import Image from 'next/image'
-
+import { Metadata } from 'next'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
-import { Metadata } from 'next'
-
 interface ProductProps {
   params: {
     slug: string
   }
 }
-
 async function getProduct(slug: string): Promise<Product> {
   const response = await api(`/products/${slug}`, {
     next: {
       revalidate: 60 * 60, // 1 hour
     },
   })
-
   const product = await response.json()
-
   return product
 }
-
 export async function generateMetadata({
   params,
 }: ProductProps): Promise<Metadata> {
@@ -29,6 +23,15 @@ export async function generateMetadata({
   return {
     title: product.title,
   }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
 }
 
 export default async function ProductPage({ params }: ProductProps) {
@@ -45,17 +48,14 @@ export default async function ProductPage({ params }: ProductProps) {
           quality={100}
         />
       </div>
-
       <div className="flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
-
         <p className="mt-2 leading-relaxed text-zinc-400">
           {product.description}
         </p>
-
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            {product.price?.toLocaleString('pt-BR', {
+            {product.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
               minimumFractionDigits: 0,
@@ -69,41 +69,38 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
         </div>
-
         <div className="mt-8 space-y-4">
           <span className="block font-semibold">Tamanhos</span>
-
           <div className="flex gap-2">
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border-zinc-700 bg-zinc-800 text-sm font-semibold hover:scale-105 transition-transform duration-500"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
             >
               P
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border-zinc-700 bg-zinc-800 text-sm font-semibold hover:scale-105 transition-transform duration-500"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
             >
               M
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border-zinc-700 bg-zinc-800 text-sm font-semibold hover:scale-105 transition-transform duration-500"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
             >
               G
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border-zinc-700 bg-zinc-800 text-sm font-semibold hover:scale-105 transition-transform duration-500"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
             >
               GG
             </button>
           </div>
         </div>
-
         <button
           type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white hover:scale-105 transition-transform duration-500"
+          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
         >
           Adicionar ao carrinho
         </button>
