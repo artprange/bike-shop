35bd9c9b2800e5218b76bd1f97259f1e45d23e6b
artprange <arthur.prange@gmail.com>
2024-08-22 16:02:11 -0300

    generating openGraph image

--- src/env.ts
+++ src/env.ts
@@ -2,17 +2,15 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  APP_URL: z.string().url(),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
-
 if (!parsedEnv.success) {
-  console.log(
-    'invalid environment variables',
+  console.error(
+    'Invalid environment variables',
     parsedEnv.error.flatten().fieldErrors,
   )
-
-  throw new Error('invalid environment variables')
+  throw new Error('Invalid environment variables.')
 }
-
 export const env = parsedEnv.data
