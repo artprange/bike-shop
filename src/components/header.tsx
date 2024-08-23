58087c8ca225377d3c2fda85099ee0dab7c9b044
artprange <arthur.prange@gmail.com>
2024-08-23 10:41:42 -0300

    deployment adjustments

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 import { CartWidget } from './cart-widget'
 import { SearchForm } from './search-form'
-
 export function Header() {
   return (
     <div className="flex items-center justify-between">
@@ -12,7 +12,9 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
